7cb594a74263eab8fc53eadd848209db7971603c
Philip Newsham <philip.newsham@hotmail.co.uk>
2017-03-30 09:38:28 +0100

    Started Updating Map

---
+++ Assets/GridMapSize.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GridMapSize : MonoBehaviour
+{
+    GridLayoutGroup m_gridLayoutGroup;
+	void Start ()
+    {
+        float width = (Screen.width * 0.6f)/12;
+        float height = (Screen.height * 0.7f)/9;
+        m_gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        m_gridLayoutGroup.cellSize = new Vector2(width, height);
+	}
+
+}
