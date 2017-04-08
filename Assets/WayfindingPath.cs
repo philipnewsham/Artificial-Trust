3e295021d97ec5c8212611ad86756033c46ba15c
Philip Newsham <philip.newsham@hotmail.co.uk>
2017-04-09 00:34:49 +0100

    Added wayfinding script

---
+++ Assets/WayfindingPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayfindingPath : MonoBehaviour
+{
+    public Transform[] wayfindingPath;
+    private int m_currentPosition = -1;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        m_currentPosition += 1;
+        if(m_currentPosition != wayfindingPath.Length)
+        {
+            transform.position = wayfindingPath[m_currentPosition].transform.position;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
